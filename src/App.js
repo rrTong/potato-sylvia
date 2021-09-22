3ac8bbc314dd2c7b9839287cd475a8b3f2962835
Ryan Tong <rrtongue@gmail.com>
2021-09-22 13:27:31 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import potato from "./images/potato.png";
+
+function App() {
+  return (
+    <div className="App">
+      <img id="potato" src={potato} />
+    </div>
+  );
+}
+
+export default App;
