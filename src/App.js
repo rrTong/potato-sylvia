8f29ffe8d7b0d9bcbdb40e18295e65f243a2bbff
Ryan Tong <rrtongue@gmail.com>
2021-09-22 18:25:34 -0700

    fix console errors with app.js

--- src/App.js
+++ src/App.js
@@ -4,8 +4,12 @@ import potato from "./images/potato.png";
 function App() {
   return (
     <div className="App">
-      <a href="https://maplestory.nexon.net/news" target="_blank">
-        <img id="potato" src={potato} />
+      <a
+        href="https://maplestory.nexon.net/news"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img id="potato" src={potato} alt="potato" />
       </a>
     </div>
   );
